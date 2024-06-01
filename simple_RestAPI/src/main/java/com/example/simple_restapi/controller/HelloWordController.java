1fb5cd853bf6988063417f43513c44c3a3da57d3
OussamaBelkadi <OussamaYC@hotmail.com>
2024-06-01 10:31:10 +0000

    add the internationonalization

--- simple_RestAPI/src/main/java/com/example/simple_restapi/controller/HelloWordController.java
+++ simple_RestAPI/src/main/java/com/example/simple_restapi/controller/HelloWordController.java
@@ -3,25 +3,33 @@ package com.example.simple_restapi.controller;
 import com.example.simple_restapi.entity.User;
 import com.example.simple_restapi.service.UserDaoService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class HelloWordController {
     private final UserDaoService userDaoService;
+    private MessageSource messageSource;
 
-    public HelloWordController(UserDaoService userDaoService) {
+
+    public HelloWordController(UserDaoService userDaoService, MessageSource messageSource) {
         this.userDaoService = userDaoService;
+        this.messageSource = messageSource;
     }
 
     // /hello-world
     @RequestMapping(method = RequestMethod.GET, path = "/hello-word")
     public String helloWord(){
-        return "Hello World";
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default message", locale);
     }
 
     @PostMapping(path = "users")
